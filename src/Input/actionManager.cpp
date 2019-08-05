032f4bc48c48311199e236becb0a0053ba93d54d
Szymon Strugala <szymonstrugala23@gmail.com>
2019-08-05 20:41:09 +0200

    Finish ActionManager class

--- src/Input/actionManager.cpp
+++ src/Input/actionManager.cpp
@@ -1,4 +1,5 @@
 #include "Input/actionManager.hpp"
+#include "Logger/logs.hpp"
 
 namespace WhitE {
 
@@ -6,6 +7,60 @@ ActionManager& ActionManager::getInstance()
 {
 	static ActionManager actionManager;
 	return actionManager;
+}
+
+void ActionManager::addAction(const std::string& actionName, sf::Keyboard::Key key)
+{
+	getInstance().mKeyboardMap.insert(std::make_pair(actionName, std::vector<sf::Keyboard::Key> {key}));
+
+	WE_LOG_INFO(actionName + " action was added to the manager.");
+}
+
+void ActionManager::addAction(const std::string& actionName, std::vector<sf::Keyboard::Key> keys)
+{
+	getInstance().mKeyboardMap.insert(std::make_pair(actionName, keys));
+}
+
+void ActionManager::addAction(const std::string& actionName, sf::Mouse::Button button)
+{
+	getInstance().mMouseMap.insert(std::make_pair(actionName, button));
+
+	WE_LOG_INFO(actionName + " action was added to the manager.");
+}
+
+void ActionManager::deleteAction(const std::string& actionName)
+{
+	if (getInstance().mKeyboardMap.find(actionName) != getInstance().mKeyboardMap.end())
+		getInstance().mKeyboardMap.erase(actionName);
+	else
+		WE_LOG_ERROR("WhitE: Key of map cannot be found!");
+}
+
+bool ActionManager::isActionPressed(const std::string& actionName)
+{
+	if (getInstance().mKeyboardMap.count(actionName))
+		return handleKeyboard(actionName) ? true : false;
+	else
+		return handleMouse(actionName) ? true : false;
+}
+
+bool ActionManager::handleKeyboard(const std::string& actionName)
+{
+	auto keys = getInstance().mKeyboardMap.at(actionName);
+	for (const auto& key : keys)
+	{
+		if (getInstance().input.isKeyPressed(key))
+			return true;
+	}
+	return false;
+}
+
+bool ActionManager::handleMouse(const std::string& actionName)
+{
+	auto buttons = getInstance().mMouseMap.at(actionName);
+	if (getInstance().input.isMouseButtonPressed(buttons))
+		return true;
+	return false;
 }
 
 }
--- src/Input/actionManager.hpp
+++ src/Input/actionManager.hpp
@@ -14,15 +14,22 @@ private:
 	ActionManager& operator=(const ActionManager&) = delete;
 
 public:
-	static void addAction(std::pair<std::string, sf::Keyboard::Key>);
-	static void addAction(std::pair<std::string, sf::Keyboard::Key>);
+	static void addAction(const std::string&, sf::Keyboard::Key);
+	static void addAction(const std::string&, std::vector<sf::Keyboard::Key>);
+	static void addAction(const std::string&, sf::Mouse::Button);
+
 	static void deleteAction(const std::string& actionName);
-	static void deleteAction(const std::string& actionName);
-	static bool isActionPressed(const std::string& actionName);
 	static bool isActionPressed(const std::string& actionName);
 
 private:
+	static bool handleMouse(const std::string& actionName);
+	static bool handleKeyboard(const std::string& actionName);
+
+private:
 	Input input;
+	std::unordered_map<std::string, std::vector<sf::Keyboard::Key>> mKeyboardMap;
+	std::unordered_map<std::string, sf::Mouse::Button> mMouseMap;
+
 	static ActionManager& getInstance();
 };
 
