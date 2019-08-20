c3504ca42262d67caab4fbabe24896f590f07476
Szymon Strugala <szymonstrugala23@gmail.com>
2019-08-20 18:38:18 +0200

    Add mock-up for ECS

---
+++ src/Objects/gameObject.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SFML/System.hpp>
+
+#include <string>
+
+namespace WhitE {
+
+class GameObject
+{
+public:
+	GameObject(const std::string name);
+
+	virtual void update(const sf::Time& deltaTime);
+	virtual void input();
+
+private:
+	const std::string mName;
+};
+
+}
